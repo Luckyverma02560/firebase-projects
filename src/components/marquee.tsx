b42152f9a822642be7672331c20ee663118b4e20
Lucky Verma. L.V <kavirajverma1976@gmail.com>
2025-12-06 12:40:27 +0000

    make a it appearing as a loop with no ending and starting. When the mess

--- src/components/marquee.tsx
+++ src/components/marquee.tsx
@@ -4,13 +4,11 @@
 const Marquee = () => {
     const separator = '\u00A0'.repeat(10);
     const message = `Instant Activation${separator}100% Uptime${separator}Secure Payment`;
-    const spacedMessage = `${message}${separator}${message}${separator}`;
 
     return (
         <div className="marquee-container">
             <div className="marquee-content">
-                <span>{spacedMessage}</span>
-                <span>{spacedMessage}</span>
+                <span>{message}</span>
             </div>
         </div>
     );
