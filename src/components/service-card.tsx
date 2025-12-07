0224cbc44f649c0c54e40935a46e515d757a973a
Lucky Verma. L.V <kavirajverma1976@gmail.com>
2025-12-07 10:56:58 +0000

    make this text position little bit right (_for element div_)

--- src/components/service-card.tsx
+++ src/components/service-card.tsx
@@ -41,7 +41,7 @@ export const ServiceCard = ({ name, logoUrl, logoHint, onButtonClick }: ServiceC
                     </svg>
                 )}
                 {name === 'Prime Video' && (
-                    <div className="flex flex-col -ml-2 h-[100px] justify-center">
+                    <div className="flex flex-col h-[100px] justify-center">
                         <span className="font-bebas-neue text-4xl tracking-wider">
                             <span className="text-prime-blue">prime</span>
                             <span className="text-white"> video</span>
