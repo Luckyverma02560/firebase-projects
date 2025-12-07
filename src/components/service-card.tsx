a631fccc5cc3bd5c56fce9ffcbcf6225e93b886a
Lucky Verma. L.V <kavirajverma1976@gmail.com>
2025-12-07 10:55:09 +0000

    make its size smaller smaller (_for element div_)

--- src/components/service-card.tsx
+++ src/components/service-card.tsx
@@ -42,7 +42,7 @@ export const ServiceCard = ({ name, logoUrl, logoHint, onButtonClick }: ServiceC
                 )}
                 {name === 'Prime Video' && (
                     <div className="flex flex-col -ml-2 h-[100px] justify-center">
-                        <span className="font-bebas-neue text-6xl tracking-wider">
+                        <span className="font-bebas-neue text-5xl tracking-wider">
                             <span className="text-prime-blue">prime</span>
                             <span className="text-white"> video</span>
                         </span>
