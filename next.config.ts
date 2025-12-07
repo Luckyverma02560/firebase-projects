4984d5167638026832f959135398cc729ea12908
Lucky Verma. L.V <kavirajverma1976@gmail.com>
2025-12-07 10:05:33 +0000

    change thisimage to image with url : https://1000logos.net/wp-content/up

--- next.config.ts
+++ next.config.ts
@@ -53,6 +53,12 @@ const nextConfig: NextConfig = {
         hostname: 'image.similarpng.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '1000logos.net',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
