45fd7d44ceaa2f5348dcc7c1b318d8ec1a901241
Lucky Verma. L.V <kavirajverma1976@gmail.com>
2025-12-08 18:24:51 +0000

    still the logo is not added, now add it with ths url : https://i.postimg

--- src/components/service-card.tsx
+++ src/components/service-card.tsx
@@ -71,7 +71,7 @@ export const ServiceCard = ({ name, logoUrl, logoHint, onButtonClick }: ServiceC
                 )}
                 {isYoutube && (
                     <div className="flex flex-col justify-center ml-4">
-                       <span className="font-sharp-sans text-3xl font-semibold tracking-wider flex items-center">
+                       <span className="font-sharp-sans text-3xl tracking-wider flex items-center">
                            <span className="text-white">YouTube</span>
                            <span className="text-youtube-red ml-1">Premium</span>
                        </span>
@@ -110,6 +110,7 @@ export const ServiceCard = ({ name, logoUrl, logoHint, onButtonClick }: ServiceC
 
 
     
+
 
 
 
