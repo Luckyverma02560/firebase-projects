d63907ba76e9deb4e3fe2ed84ac1e94874f5054d
Lucky Verma. L.V <kavirajverma1976@gmail.com>
2025-12-13 17:41:58 +0000

    Remove this (_for element <LinkComponent>_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,30 +21,12 @@ const homeLink = { href: '/', label: 'Home' };
 const plansLink = { href: '/about', label: 'Plans' };
 const getStartedLink = { href: '#', label: 'Get Started' };
 const cartLink = { href: '/cart', label: 'View Cart' };
-const whatsappLink = { href: 'https://wa.me/918600070638', label: 'Whatsapp' };
 
 
 const navLinks = [
   { href: '#', label: 'Payment' },
   { href: '#', label: 'Contact Us' },
 ];
-
-const WhatsAppIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
-    </svg>
-);
 
 export default function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -149,12 +131,6 @@ export default function Header() {
                     <span className="sr-only">{cartLink.label}</span>
                 </Link>
             </Button>
-            <Button asChild variant="outline" size="icon" className="text-white border-green-500 bg-green-500/10 hover:bg-green-500/20 hover:text-white transition-colors duration-300">
-                <Link href={whatsappLink.href} target="_blank">
-                    <WhatsAppIcon />
-                    <span className="sr-only">{whatsappLink.label}</span>
-                </Link>
-            </Button>
           </div>
 
           <div className="md:hidden flex-1 flex justify-end">
@@ -173,12 +149,6 @@ export default function Header() {
                         <Link href={cartLink.href} className="flex items-center justify-center gap-2 py-3 text-xl text-center font-headline rounded-md text-foreground">
                             <ShoppingCart />
                             <span>{cartLink.label}</span>
-                        </Link>
-                    </SheetClose>
-                     <SheetClose asChild>
-                        <Link href={whatsappLink.href} target="_blank" className="flex items-center justify-center gap-2 py-3 text-xl text-center font-headline rounded-md text-green-500">
-                            <WhatsAppIcon />
-                            <span>{whatsappLink.label}</span>
                         </Link>
                     </SheetClose>
                     <div className="pt-4">
