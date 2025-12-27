8b4474f158c4bd3e65fd0798232c8a398c09eeac
Luckyverma02560 <139264444+Luckyverma02560@users.noreply.github.com>
2025-12-27 15:06:13 +0530

    Refactor RootLayout and update metadata structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,68 @@
-export const metadata = {
-  verification: {
-    google: "SU7TF2XEeS_xzsBUaIteDHVGqZ0DsZ6Gr7Y3Ad_W66A",
-  },
+import type {Metadata} from 'next';
+import { Cinzel, Inter, Noto_Serif, PT_Sans, Playfair_Display } from 'next/font/google';
+import './globals.css';
+import { Toaster } from "@/components/ui/toaster";
+import Header from '@/components/header';
+import Footer from '@/components/footer';
+import { ScrollToTopButton } from '@/components/scroll-to-top-button';
+import { CartProvider } from '@/context/cart-context';
+import { ServiceProvider } from '@/context/service-context';
+import { cn } from '@/lib/utils';
+
+const ptSans = PT_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-pt-sans',
+});
+
+const playfairDisplay = Playfair_Display({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  variable: '--font-playfair-display',
+});
+
+const cinzel = Cinzel({
+  subsets: ['latin'],
+  weight: ['700'],
+  variable: '--font-cinzel',
+});
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
+  variable: '--font-inter',
+});
+
+const notoSerif = Noto_Serif({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    variable: '--font-noto-serif',
+});
+
+
+export const metadata: Metadata = {
+  title: 'EL11VEN HUB',
+  description: 'Your Gateway to Ultimate Streaming Experience',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="en">
-      <head />
-      <body>{children}</body>
+    <html lang="en" className="!scroll-smooth dark" suppressHydrationWarning>
+      <body className={cn("font-body antialiased", ptSans.variable, playfairDisplay.variable, cinzel.variable, inter.variable, notoSerif.variable)} suppressHydrationWarning>
+        <ServiceProvider>
+          <CartProvider>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+            <Toaster />
+            <ScrollToTopButton />
+          </CartProvider>
+        </ServiceProvider>
+      </body>
     </html>
   );
 }
